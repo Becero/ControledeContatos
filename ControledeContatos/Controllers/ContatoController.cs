7e231a7e45521a519bb0ffc181fc793d7326cf77
Luis Guilherme <51214414+Becero@users.noreply.github.com>
2023-05-01 20:02:16 -0300

    Adicionar arquivos de projeto.

---
+++ ControledeContatos/Controllers/ContatoController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ControledeContatos.Controllers
+{
+    public class ContatoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Criar()
+        {
+            return View();
+        }
+
+        public IActionResult Editar()
+        {
+            return View();
+        }
+        public IActionResult ApagarConfirmacao()
+        {
+            return View();
+        }
+    }
+}
