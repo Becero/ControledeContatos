4f7df361f4b290e1ee1ad38595f22e9b55960fc6
Luis Guilherme <51214414+Becero@users.noreply.github.com>
2023-05-26 11:12:13 -0300

    senha

--- ControledeContatos/Helper/Email.cs
+++ ControledeContatos/Helper/Email.cs
@@ -1,4 +1,7 @@
-﻿namespace ControledeContatos.Helper
+﻿using System.Net;
+using System.Net.Mail;
+
+namespace ControledeContatos.Helper
 {
     public class Email : IEmail
     {
@@ -12,13 +15,36 @@
         {
            try
             {
+                string host = _configuration.GetValue<string>("SMTP:Host");
+                string nome = _configuration.GetValue<string>("SMTP:Nome");
+                string username = _configuration.GetValue<string>("SMTP:UserName");
+                string senha = _configuration.GetValue<string>("SMTP:Senha");
+                int porta = _configuration.GetValue<int>("SMTP:Porta");
 
+                MailMessage mail = new MailMessage()
+                {
+                    From = new MailAddress(username, nome)
+                };
+
+                mail.To.Add(email);
+                mail.Subject = assunto;
+                mail.Body = mensagem;
+                mail.IsBodyHtml = true;
+                mail.Priority = MailPriority.High;
+
+                using (SmtpClient smtp = new SmtpClient(host, porta))
+                {
+                    smtp.Credentials = new NetworkCredential(username, senha);
+                    smtp.EnableSsl = true;
+
+                    smtp.Send(mail);
+                    return true;
+                }
             }
-            catch(SystemException ex) 
+            catch(System.Exception ex) 
             {
                 //Gravar log de erro ao enviar e-mail
                 return false;
-
             }
         }
     }
